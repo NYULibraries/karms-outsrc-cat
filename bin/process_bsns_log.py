ceaa73d9ea15c46e11f4ce8fc7ebec1da9b5661a
Heidi Frank <hfrank71@gmail.com>
2016-08-17 05:39:23 -0400

    Fix script to append bsns_log timestamp not overwrite

--- bin/process_bsns_log.py
+++ bin/process_bsns_log.py
@@ -13,10 +13,10 @@ print current_timestamp
 
 parent_dir = os.path.dirname(os.getcwd())
 
-process = raw_input('Enter the process (e.g., add new batch, send to vendor, return from vendor, load to Aleph): ')
-vendor = raw_input('Enter the vendor name (TechPro or BackStage): ')
+process = raw_input('Enter the process code - (A)dd new batch, (S)end to vendor, (R)eturn from vendor, (L)oad to Aleph: ')
+vendor = raw_input('Enter the vendor code - TechPro or BackStage: ')
 
-if process == 'add new batch' or process == 'send to vendor':
+if process == 'A' or process == 'S':
 	batch_name = raw_input('Enter the batch name (e.g., tp_20160121_bk_gre_14r): ')
 	vendor_code = batch_name.split('_')[0]
 	if vendor_code == 'tp':
@@ -42,25 +42,30 @@ bsns_csv_file_in.close()
 match = False
 matched_cnt = 0
 not_matched_cnt = 0
+bsns_proc_cnt = 0
 
 # Method: Add new batch of BSNs to log file
-if process == 'add new batch':
+if process == 'A':
 	new_bsns_file = open(batch_folder+batch_name+'_new_bsns', 'r')
 	new_bsns = new_bsns_file.readlines()
 	for new_bsn in new_bsns:
+		bsns_proc_cnt += 1
 		new_row = [new_bsn.rstrip(),batch_name,'',current_timestamp,'','','']
 		bsns_lines.append(new_row)
 
 	
 # Method: Add status date for when BSNs are processed and "sent to vendor"
-if process == 'send to vendor':
+if process == 'S':
 	final_marc_recs = pymarc.MARCReader(file(batch_folder+batch_name+'_pkgd_marc_final.mrc'), to_unicode=True, force_utf8=True)
 	for f_marc_rec in final_marc_recs:
+		bsns_proc_cnt += 1
 		f_marc_rec_001 = f_marc_rec.get_fields('001')[0].value() + 'NYU01'
 		for line in bsns_lines:
 			if line[0] == f_marc_rec_001:
 				match = True
-				line[4] = current_timestamp
+				if len(line[4]) > 0:
+					line[4] += ' | '
+				line[4] += current_timestamp
 		if match:
 			print f_marc_rec_001 + ' was MATCHED in the BSNs log'
 			matched_cnt += 1
@@ -69,24 +74,31 @@ if process == 'send to vendor':
 			not_matched_cnt += 1
 
 # Method: Match returned file of vendor BSNs and add status date for when BSNs are processed as "returned from vendor" or as "loaded to Aleph"
-if process == 'return from vendor' or process == 'load to Aleph':
+if process == 'R' or process == 'L':
 	marc_returned_folder = parent_dir+'/submissions/'+vendor+'/marc_returned/'
 	marc_returned_filenames = os.listdir(marc_returned_folder)
 	for filename in marc_returned_filenames:
-		if filename.endswith('.mrc'):
+		if filename.endswith('.mrc') or filename.endswith('.MRC'):
 			print filename
 	vendor_filename = raw_input('Enter the .mrc filename for the returned file of vendor records to be logged: ')
 	vendor_marc_recs = pymarc.MARCReader(file(marc_returned_folder+vendor_filename), to_unicode=True, force_utf8=True)
 	for v_marc_rec in vendor_marc_recs:
+		bsns_proc_cnt += 1
 		v_marc_rec_001 = v_marc_rec.get_fields('001')[0].value() + 'NYU01'
 		for line in bsns_lines:
 			if line[0] == v_marc_rec_001:
 				match = True
-				if process == 'return from vendor':
-					line[2] = vendor_filename
-					line[5] = current_timestamp
-				elif process == 'load to Aleph':
-					line[6] = current_timestamp
+				if process == 'R':
+					if len(line[2]) > 0:
+						line[2] += ' | '
+					line[2] += vendor_filename
+					if len(line[5]) > 0:
+						line[5] += ' | '
+					line[5] += current_timestamp
+				elif process == 'L':
+					if len(line[6]) > 0:
+						line[6] += ' | '
+					line[6] += current_timestamp
 		if match:
 			print v_marc_rec_001 + ' was MATCHED in the BSNs log'
 			matched_cnt += 1
@@ -106,5 +118,8 @@ bsns_log_out.writerow(bsns_lines_header)
 bsns_log_out.writerows(bsns_lines_sorted)
 bsns_csv_file_out.close()
 
-
+print '---------------------------------------------'
+print 'Total BSNs processed: '+str(bsns_proc_cnt)
+print 'Total Matched BSNs: '+str(matched_cnt)
+print 'Total UN-Matched BSNs: '+str(not_matched_cnt)
 
